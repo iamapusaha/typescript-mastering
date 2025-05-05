05a8416ffb8663e37b29e5183872b87ddb65dcbe
iamapusaha <apusaha392@gmail.com>
2025-05-05 17:10:26 +0600

    added arrow function

--- module-01/src/function.ts
+++ module-01/src/function.ts
@@ -1,4 +1,6 @@
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number): number {
   return num1 + num2;
 }
 add(2, 3);
+
+const add2 = (num1: number, num2: number): number => num1 + num2;
