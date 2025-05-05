3f794fbf069273816027d0cc3fbdd20b0de2523c
iamapusaha <apusaha392@gmail.com>
2025-05-05 17:23:07 +0600

    added method in ts

---
+++ module-01/dist/function.js
@@ -0,0 +1,16 @@
+"use strict";
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(2, 3);
+const add2 = (num1, num2) => num1 + num2;
+const richUser = {
+    name: "akash",
+    age: 29,
+    balance: 89,
+    addBalance(balance) {
+        return `This is my last bank blance ${balance + this.balance}`;
+    },
+};
+const ful = richUser.addBalance(2566);
+console.log(ful);
--- module-01/src/function.ts
+++ module-01/src/function.ts
@@ -4,3 +4,14 @@ function add(num1: number, num2: number): number {
 add(2, 3);
 
 const add2 = (num1: number, num2: number): number => num1 + num2;
+
+const richUser = {
+  name: "akash",
+  age: 29,
+  balance: 89,
+  addBalance(balance: number): string {
+    return `This is my last bank blance ${balance + this.balance}`;
+  },
+};
+const ful = richUser.addBalance(2566);
+console.log(ful);
