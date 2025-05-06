8cfdfefac36dad28eb32a877f15d0a23709b01aa
iamapusaha <apusaha392@gmail.com>
2025-05-06 13:42:40 +0600

    object distructuring

---
+++ module-01/src/distructuring.ts
@@ -0,0 +1,23 @@
+{
+  const user = {
+    salar: "5k",
+    exprencies: "10y",
+    id: 89436,
+    name: {
+      firstName: "apu",
+      middleName: "kumar",
+      lastName: "saha",
+    },
+    goal: "biya kora",
+  };
+
+  const {
+    salar,
+    name: { firstName: nickName },
+  } = user;
+  console.log(salar, nickName);
+
+  const myFriends: string[] = ["akash", "batas", "kajol", "mahti"];
+
+  const [a, b, x, ...rest] = myFriends;
+}
