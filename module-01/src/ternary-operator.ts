0faaadebaeecf73f79cf6ea1d0e9502f535650b7
iamapusaha <apusaha392@gmail.com>
2025-05-06 23:54:14 +0600

    tested ternary operator

---
+++ module-01/src/ternary-operator.ts
@@ -0,0 +1,11 @@
+{
+  const age = 15;
+  if (age >= 18) {
+    console.log("you are ready to biya");
+  } else {
+    console.log("tor beta biyar boyos hoi nai");
+  }
+
+  const isAdult = age >= 18 ? "adult" : "not adult";
+  console.log(isAdult);
+}
