2e8a42b7817ff44be6adc7732a6aa0bd3087f8de
iamapusaha <apusaha392@gmail.com>
2025-05-06 21:12:49 +0600

    type alias

---
+++ module-01/src/type-alias.ts
@@ -0,0 +1,28 @@
+type Person = {
+  name: string;
+  age: string;
+  position: string;
+  batch: number;
+  joiningDate: number;
+};
+const person: Person = {
+  name: "mokbul",
+  age: "27",
+  position: "jr software enginer",
+  batch: 985,
+  joiningDate: 27 - 12 - 26,
+};
+const person1: Person = {
+  name: "mokbul",
+  age: "27",
+  position: "jr software enginer",
+  batch: 985,
+  joiningDate: 27 - 12 - 26,
+};
+const person3: Person = {
+  name: "mokbul",
+  age: "27",
+  position: "jr software enginer",
+  batch: 985,
+  joiningDate: 27 - 12 - 26,
+};
