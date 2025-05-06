d7d3c8c65e5165a4125e672dca0d42751d472c9c
iamapusaha <apusaha392@gmail.com>
2025-05-06 22:59:27 +0600

    type alias for function

--- module-01/src/type-alias.ts
+++ module-01/src/type-alias.ts
@@ -1,28 +1,33 @@
-type Person = {
-  name: string;
-  age: string;
-  position: string;
-  batch: number;
-  joiningDate: number;
-};
-const person: Person = {
-  name: "mokbul",
-  age: "27",
-  position: "jr software enginer",
-  batch: 985,
-  joiningDate: 27 - 12 - 26,
-};
-const person1: Person = {
-  name: "mokbul",
-  age: "27",
-  position: "jr software enginer",
-  batch: 985,
-  joiningDate: 27 - 12 - 26,
-};
-const person3: Person = {
-  name: "mokbul",
-  age: "27",
-  position: "jr software enginer",
-  batch: 985,
-  joiningDate: 27 - 12 - 26,
-};
+{
+  type Person = {
+    name: string;
+    age: string;
+    position: string;
+    batch: number;
+    joiningDate: number;
+  };
+  const person: Person = {
+    name: "mokbul",
+    age: "27",
+    position: "jr software enginer",
+    batch: 985,
+    joiningDate: 27 - 12 - 26,
+  };
+  const person1: Person = {
+    name: "mokbul",
+    age: "27",
+    position: "jr software enginer",
+    batch: 985,
+    joiningDate: 27 - 12 - 26,
+  };
+  const person3: Person = {
+    name: "mokbul",
+    age: "27",
+    position: "jr software enginer",
+    batch: 985,
+    joiningDate: 27 - 12 - 26,
+  };
+  type Add = (num1: number, num2: number) => number;
+
+  const add: Add = (num1, num2) => num1 * num2;
+}
