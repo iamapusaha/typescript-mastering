12d7836a6fb2c96be78f930266e53c340c2d3327
iamapusaha <apusaha392@gmail.com>
2025-05-06 23:12:11 +0600

    union types

---
+++ module-01/src/union-types.ts
@@ -0,0 +1,17 @@
+type FrontendDeveloper = "fokiraDeveloper" | "expreinessDeveloper";
+
+const pHeroMake: FrontendDeveloper = "expreinessDeveloper";
+
+type User = {
+  name: string;
+  age: number;
+  bloodGroup: "A+" | "B+" | "Ab+";
+  gender: "male" | "female";
+};
+
+const gpeople: User = {
+  name: "abbas ali",
+  age: 72,
+  bloodGroup: "A+",
+  gender: "male",
+};
